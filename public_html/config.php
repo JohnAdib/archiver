10a480cc017ac56b1b9dd9192c7512e63b2e8075
Javad <J.Evazzadeh@gmail.com>
2014-12-20 17:11:43 +0330

    1

---
+++ public_html/config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ @ In the name Of Allah
+ * The base configurations of the SAMAC.
+ * This file has the configurations of MySQL settings and useful core settings
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database */
+// dbconnection_lib::$db_name 		= 'jibres';
+
+/** MySQL database username */
+// dbconnection_lib::$db_user 		= 'jibres';
+
+/** MySQL database password */
+// dbconnection_lib::$db_password 	= 'Jibres@#$567';
+
+
+/**
+ * For developers: debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use DEBUG
+ * in their development environments.
+ */
+define('DEBUG', true);
+?>
