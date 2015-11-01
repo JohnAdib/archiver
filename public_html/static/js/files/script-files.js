de016c40462a6dec00229a364fc4c4f6862ed503
Ahmad <ahmadkarimi1991@gmail.com>
2015-11-01 18:33:58 +0330

    selection functionallity improved

--- public_html/static/js/files/script-files.js
+++ public_html/static/js/files/script-files.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
 			console.log(1);
 		} else if (e.ctrlKey) {
 			$(this).toggleClass('selected');
+			console.log('ctrl');
 		} else if(e.shiftKey) {
 			console.log(3)
 		} else {
@@ -38,6 +39,7 @@ $(document).ready(function() {
 	});
 
 	$(document).keydown(function(e) {
+		console.log(e);
         if (e.ctrlKey && e.keyCode == 65) {
         	if ($('#explorer>ul').hasClass('select-all'))  {
     			$('#explorer>ul li').removeClass('selected');
@@ -50,6 +52,47 @@ $(document).ready(function() {
         	}
 
         	$('#explorer>ul').toggleClass('select-all');
+        }
+
+		var last    = parseInt($('#explorer>ul li:last').attr('data-row'));
+		var current = parseInt($('#explorer>ul li.focused').attr('data-row'));
+
+        switch(e.which) {
+        	case 17: // ctrl
+
+        	break;
+
+        	case 32: // space
+        		if (e.ctrlKey) {
+        			$('#explorer>ul li').eq(current).toggleClass('selected');
+        		} else {
+        			$('#explorer>ul li').eq(current).addClass('selected');
+        		}
+        	break;
+            
+            case 37: // left
+            	console.log('left');
+            break;
+
+            case 38: // up
+            	next = (current) > 0 ? current - 1 : 0;
+            	$('#explorer>ul li').removeClass('focused selected');
+            	$('#explorer>ul li').eq(next).addClass('focused selected');
+            break;
+
+            case 39: // right
+            	console.log('right');
+            break;
+
+            case 40: // down
+            	next = (current + 1) > last ? last : current + 1;
+            	$('#explorer>ul li').removeClass('focused selected');
+            	$('#explorer>ul li').eq(next).addClass('focused selected');
+            break;
+
+            default:
+            // console.log(e.which);
+             return; // exit this handler for other keys
         }
     });
 });
