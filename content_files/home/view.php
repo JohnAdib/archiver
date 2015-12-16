65146311b02527da4ed865e1ca2391942d51326a
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2015-12-16 16:28:17 +0330

    fix view

--- content_files/home/view.php
+++ content_files/home/view.php
@@ -11,23 +11,25 @@ class view extends \mvc\view
 		$this->include->lightbox    = true;
 
 		$this->data->bodyclass      = 'unselectable';
-		
+
 		$this->data->location       = $this->url('path', -1);
-		
+
 		// $this->global->tree      = json_encode($this->model()->tree());
 		// $this->data->datatable      = $this->model()->draw($this->url->path);
 		$this->data->datatable      = $this->model()->draw();
-		if($this->data->location == '' && count($this->data->datatable) == 0)
+		// var_dump(count($this->data->datatable) );
+		if(count($this->data->location) == 0 && count($this->data->datatable) == 0)
 		{
+			// var_dump(12333);
 			$this->data->bodyclass = $this->data->bodyclass. ' first-time';
 		}
-		
+
 		$this->data->site['title']  = T_("Archiver");
 		$this->data->site['desc']   = T_("Archiver is new");
 		$this->data->site['slogan'] = T_("Ermile is our company");
 		$this->data->dir['right']   = $this->global->direction == 'rtl'? 'left':  'right';
 		$this->data->dir['left']    = $this->global->direction == 'rtl'? 'right': 'left';
-		
+
 		$this->data->page['desc']   = T_("Archiver is another archive system!");
 
 		$this->data->maxSize = \lib\utility\Upload::max_file_upload_in_bytes();
