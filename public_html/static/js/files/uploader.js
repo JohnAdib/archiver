26cd53c300288ff79bfdb3808ebb88f53f6e619f
Javad <J.Evazzadeh@live.com>
2015-12-19 16:12:17 +0330

    fix uploader notify

--- public_html/static/js/files/uploader.js
+++ public_html/static/js/files/uploader.js
@@ -32,8 +32,8 @@ route('*', function()
     add: function (e, data)
     {
 
-      var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
-        ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span></span><i></i></li>');
+      var tpl = $('<li class="working"><input type="text" value="0" data-width="25" data-height="25"'+
+        ' data-fgColor="#03A9F4" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span></span><i></i></li>');
 
       // Append the file name and file size
       tpl.find('p').text(data.files[0].name).attr('title', data.files[0].name)
