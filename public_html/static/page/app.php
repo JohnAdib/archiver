0a6d706e2999b092d9a50d834880455f23e20613
Javad <J.Evazzadeh@live.com>
2016-01-25 16:06:43 +0330

    user can set key and value of special app for test

--- public_html/static/page/app.php
+++ public_html/static/page/app.php
@@ -3,12 +3,17 @@
  <head>
   <meta charset="UTF-8"/>
   <style type="text/css">
-  	body{background-color: #ddd;text-align: center; font-family: "IranSans", "B Nazanin", Tahoma}
+   *, *:before, *:after{margin:0;padding:0;outline:none !important;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;}
+  	body{background-color: #ddd;text-align: center; font-family: "IranSans", "B Nazanin", sans-serif}
   	h1,h2{margin:50px 20px 20px;}
-  	form{margin:20px 10px;}
-  	input{padding:10px 30px;background-color:#ddd;}
+  	form{margin:20px auto;width:40%}
+  	input{padding:7px 20px;background-color:#ddd;min-width: 100px;margin:5px 0}
+    input[type="text"]:focus{background-color: #eee}
+    input[name="result"]{display:block;width: 100%}
     img{max-height:300px;display:block; margin:0 auto;}
     code{font-family:monospace,monospace;font-size:0.7em;margin: 10px 5%;display:block}
+    input.key{width: 30%;}
+    input.value{width: 60%;}
   </style>
  </head>
 
@@ -60,13 +65,13 @@
 
 
   <form method="get" name="analyse" action="http://files.<?php echo $_SERVER['HTTP_HOST']; ?>/$/result">
-   <input type="hidden" name="key1" value="location">
-   <input type="hidden" name="value1" value="sea">
+   <input type="text" class="key" name="key1" value="location">
+   <input type="text" class="value" name="value1" value="sea">
 
-   <input type="hidden" name="key2" value="app">
-   <input type="hidden" name="value2" value="<?php echo $_GET['app']; ?>">
+   <input type="text" class="key" name="key2" value="app">
+   <input type="text" class="value" name="value2" value="<?php echo $_GET['app']; ?>">
 
-   <input type="hidden" name="result" value="return custom result! this is text. به فارسی هم میتواند پاسخ دهد!">
+   <input type="text" name="result" value="return custom result! this is text. به فارسی هم میتواند پاسخ دهد!">
    <input type="hidden" name="authcode" value="<?php echo $_GET['authcode']; ?>">
    <input type="submit">
   </form>
