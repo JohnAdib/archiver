57c78b0ec4c5cf94cce2400c735b8e173501dbf0
Javad <J.Evazzadeh@live.com>
2016-04-05 11:30:55 +0430

    fix to work with last update

--- public_html/index.php
+++ public_html/index.php
@@ -3,16 +3,6 @@
  @ In the name Of Allah
 **/
 
-// if personal config exist, require it
-if ( file_exists( __DIR__ . '/config.me.php') )
-	require_once( __DIR__ . '/config.me.php');
-// elseif config exist, require it else show related error message
-elseif ( file_exists( __DIR__ . '/config.php') )
-	require_once( __DIR__ . '/config.php');
-else
-{   // A config file doesn't exist
-	exit("<p>There doesn't seem to be a <code>config.php</code> file. I need this before we can get started.</p>");
-}
 // if Saloos exist, require it else show related error message
 if ( file_exists( '../../saloos/autoload.php') )
 {
